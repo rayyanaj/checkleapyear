ab0aa4616472af006fe533c8c1ebe5015cec8043
Rayyan Abduljawad <104502377+rayyanaj@users.noreply.github.com>
2022-04-27 15:40:34 +0400

    Add files via upload

---
+++ checkleapyear.py
@@ -0,0 +1,23 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+def IsLeap(year):  
+  # Checking if the given year is leap year  
+  if((year % 100 != 0) or  # to check whether it's a century year or not
+     (year % 400 == 0) and # to check whether century year is leap year
+     (year % 4 == 0)):   # to check whether any year is leap year
+    print(year, "is a leap year");  
+  # Else it is not a leap year  
+  else:  
+    print (year, "is not a leap year")  
+# Taking an input year from user  
+year = int(input("Enter the year: "))  
+  
+# Printing result  
+IsLeap(year)  
+    
