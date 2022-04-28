d9a9908ed51c00330d52068a59dfefdcac432535
Rayyan Abduljawad <104502377+rayyanaj@users.noreply.github.com>
2022-04-28 09:00:40 +0400

    Update checkleapyear.py

--- checkleapyear.py
+++ checkleapyear.py
@@ -1,7 +1,7 @@
 def IsLeap(year):  
   # Checking if the given year is leap year  
-  if((year % 100 != 0) or  # to check whether it's a century year or not
-     (year % 400 == 0) and # to check whether century year is leap year
+  if((year % 400 == 0) or # to check whether century year is leap year or  
+     (year % 100 != 0) and # to check whether it's a century year or not
      (year % 4 == 0)):   # to check whether any year is leap year
     print(year, "is a leap year");  
   # Else it is not a leap year  
@@ -11,5 +11,4 @@ def IsLeap(year):
 year = int(input("Enter the year: "))  
   
 # Printing result  
-IsLeap(year)  
-    
+IsLeap(year)
